0a3c6a2828c66bf47bb787359d97896cbba67102
jocysainz <lauragrajeda29@yahoo.com>
2026-01-10 00:14:18 -0600

    Guess the number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("==============================================");
+        Console.WriteLine("        WELCOME TO GUESS MY NUMBER!!!         ");
+        Console.WriteLine("==============================================");
+        Console.ResetColor();
+        Console.WriteLine();
+
+        string playAgain;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess;
+            int guessCount = 0;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("I have chosen a number between 1 and 100. Try to guess it!");
+            Console.ResetColor();
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("What is your guess? ");
+                Console.ResetColor();
+
+                string input = Console.ReadLine();
+                guess = int.Parse(input);
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Higher");
+                    Console.ResetColor();
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Lower");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine($"It took you {guessCount} guess{(guessCount > 1 ? "es" : "")}.");
+                    Console.ResetColor();
+                }
+
+            } while (guess != magicNumber);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Do you want to play again? (yes/no) ");
+            Console.ResetColor();
+            playAgain = Console.ReadLine().ToLower();
+            Console.WriteLine();
+
+        } while (playAgain == "yes");
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Thanks for playing! Goodbye.");
+        Console.ResetColor();
     }
 }
