5a1a418c2c71bd1448b58f2b6a3ea5f996c0cc12
jocysainz <lauragrajeda29@yahoo.com>
2026-01-30 21:18:59 -0600

    Week 4

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("127 Sky St", "Conroe", "TX", "USA");
+        Customer customer1 = new Customer("Melanie Sainz", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Heart Balloons", "H120", 15.00, 5));
+        order1.AddProduct(new Product("Necklace", "M226", 25.00, 1));
+
+        Address address2 = new Address("117 Hidalgo", "Obregon", "SN", "Mexico");
+        Customer customer2 = new Customer("Laura Grajeda", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Monitor", "LG202", 273.89, 1));
+        order2.AddProduct(new Product("HDMI Cable", "C111", 10.89, 1));
+
+        DisplayOrder(order1);
+        Console.WriteLine();
+        DisplayOrder(order2);
+    }
+
+    static void DisplayOrder(Order order)
+    {
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order.GetTotalPrice():0.00}");
     }
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        Video video1 = new Video("Fairytopia", "Princess123", 965);
+        Video video2 = new Video("Living alone for 100 days!!", "Mr.Beat", 750);
+        Video video3 = new Video("R.E.P.O", "CaseOhn", 455);
+
+        video1.AddComment(new Comment("Becca123", "I love watching Barbie movies!"));
+        video1.AddComment(new Comment("VeroSS", "I can't believe that actually happened"));
+        video1.AddComment(new Comment("Leon", "Seems like I need to download this movie..."));
+
+        video2.AddComment(new Comment("Brand0n17", "I can totally do this now problem"));
+        video2.AddComment(new Comment("Jocygs", "WILD"));
+        video2.AddComment(new Comment("Fiona_00", "For that money, I would do it too!"));
+
+        video3.AddComment(new Comment("Pr0om97", "Me and my kids play this on repeat"));
+        video3.AddComment(new Comment("Hannahhhh", "It can be kind of scary at times :("));
+        video3.AddComment(new Comment("Ian1999", "I need to get more xp for my character"));
+
+        List<Video> videos = new List<Video>
+        {
+            video1,
+            video2,
+            video3
+        };
+
+        foreach (Video video in videos)
+        {
+            Console.WriteLine($"Title: {video.Title}");
+            Console.WriteLine($"Author: {video.Author}");
+            Console.WriteLine($"Length: {video.LengthInSeconds} seconds");
+            Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
+
+            Console.WriteLine("Comments:");
+            foreach (Comment comment in video.GetComments())
+            {
+                Console.WriteLine($" - {comment.CommenterName}: {comment.Text}");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
